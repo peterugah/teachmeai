b4eb70c515670d9db6eb30834c531afb19c79eca
admin <admin@MBP-de-admin.fritz.box>
2025-04-11 00:51:51 +0200

    feat: setup

--- src/pages/content/main.tsx
+++ src/pages/content/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "../../index.css";
+import { Working } from ".";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<p>working content</p>
+		<p>working</p>
+		<Working />
 	</StrictMode>
 );
