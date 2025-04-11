8824aca8464603575cceff835de59562388ba020
admin <admin@MBP-de-admin.fritz.box>
2025-04-11 15:36:07 +0200

    feat: wip

--- src/pages/content/Content.tsx
+++ src/pages/content/Content.tsx
@@ -3,6 +3,8 @@ import {
 	HandThumbUpIcon,
 	SpeakerWaveIcon,
 } from "@heroicons/react/24/outline";
+import ReactMarkDown from "react-markdown";
+import remarkGfm from "remark-gfm";
 interface Props {
 	title: string;
 	content: string;
@@ -11,7 +13,15 @@ export const Content = ({ title, content }: Props) => {
 	return (
 		<div className="mt-4">
 			<h1 className={`font-bold mb-2`}>{title}</h1>
-			<p>{content}</p>
+			{/* <p>{content}</p> */}
+			<ReactMarkDown
+				components={{
+					p: ({ children }) => <p className="text-blue-700 mb-2">{children}</p>,
+				}}
+				remarkPlugins={[remarkGfm]}
+			>
+				{content}
+			</ReactMarkDown>
 			<div className="mt-4 flex items-center space-x-2">
 				<button className="cursor-pointer text-gray-800 hover:text-black size-7 hover:bg-gray-100 p-1 rounded-[5px]">
 					<HandThumbUpIcon />
--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -18,10 +18,67 @@ export function Index() {
 				/>
 				<Content
 					title="Explanation"
-					content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, fugit
-				repudiandae vel aliquid amet saepe cum? Sapiente at, accusantium minus
-				adipisci eaque nemo explicabo voluptatem tempore eligendi nobis dolorum
-				harum."
+					content="# My Markdown Example
+
+## Introduction
+
+This is a sample markdown file that demonstrates the basic syntax and features of markdown. Markdown is a lightweight markup language with plain-text formatting syntax.
+
+## Table of Contents
+
+1. [Introduction](#introduction)
+2. [Headers](#headers)
+3. [Lists](#lists)
+4. [Links](#links)
+5. [Images](#images)
+6. [Code Blocks](#code-blocks)
+7. [Blockquotes](#blockquotes)
+
+## Headers
+
+Markdown allows you to create headers by adding hash symbols (`#`) at the beginning of a line. The number of hashes corresponds to the header level.
+
+### Header 3
+
+#### Header 4
+
+##### Header 5
+
+## Lists
+
+You can create ordered and unordered lists in markdown.
+
+- Unordered List Item 1
+- Unordered List Item 2
+  - Nested Unordered List Item
+  - Nested Unordered List Item 2
+- Unordered List Item 3
+
+1. Ordered List Item 1
+2. Ordered List Item 2
+3. Ordered List Item 3
+
+## Links
+
+You can create links using the following syntax:
+
+[Google](https://www.google.com)
+
+## Images
+
+You can embed images using this syntax:
+
+![Image Alt Text](https://via.placeholder.com/150)
+
+## Code Blocks
+
+For inline code, use backticks:
+
+`inline code`
+
+For multi-line code blocks, use triple backticks:
+
+"
 				/>
 				<AskMore />
 			</div>
