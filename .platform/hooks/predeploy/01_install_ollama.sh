d84a6ffd2bc86c8b926f354a46e3d0acc1449e1e
peterugah <p.ugah.pu@gmail.com>
2025-07-24 20:22:28 +0200

    feat: add ollama

---
+++ .platform/hooks/predeploy/01_install_ollama.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+# 1) install ollama
+curl -fsSL https://ollama.com/install.sh | sh
+
+# 2) pull models
+/usr/local/bin/ollama pull mistral
+/usr/local/bin/ollama pull nomic-embed-text
+
+# 3) enable service on startup
+systemctl enable ollama
