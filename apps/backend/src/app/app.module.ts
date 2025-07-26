e728ae8911ed7c35d79d5aff5ba58e619ad4f341
peterugah <p.ugah.pu@gmail.com>
2025-07-26 11:39:29 +0200

    feat: add ollama

---
+++ apps/backend/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { SearchModule } from '../search/search.module';
+import { DatabaseModule } from '../database/database.module';
+import { EnvModule } from '../env/env.module';
+import { UserModule } from '../user/user.module';
+import { ReportModule } from '../report/report.module';
+import { CronjobModule } from '../cronjob/cronjob.module';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [SearchModule, DatabaseModule, EnvModule, UserModule, ReportModule, CronjobModule],
+  controllers: [AppController],
+})
+export class AppModule { }
